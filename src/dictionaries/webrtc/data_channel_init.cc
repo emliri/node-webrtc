011b122dbab827ce01f009e8fc5bd0c1d6e32654
Stephan Hesse <stephan@emliri.com>
2020-01-03 19:15:28 +0100

    data_channel_init.cc: fix characteristic of dictionary fields to have defaults with meaningful value for the data-type and given the defaults documented as "null".

--- src/dictionaries/webrtc/data_channel_init.cc
+++ src/dictionaries/webrtc/data_channel_init.cc
@@ -15,8 +15,8 @@ namespace node_webrtc {
 #define DATA_CHANNEL_INIT_FN CreateDataChannelInit
 #define DATA_CHANNEL_INIT_LIST \
   DICT_DEFAULT(bool, ordered, "ordered", true) \
-  DICT_OPTIONAL(uint32_t, maxPacketLifeTime, "maxPacketLifeTime") \
-  DICT_OPTIONAL(uint32_t, maxRetransmits, "maxRetransmits") \
+  DICT_DEFAULT(uint32_t, maxPacketLifeTime, "maxPacketLifeTime", 0) \
+  DICT_DEFAULT(uint32_t, maxRetransmits, "maxRetransmits", 0) \
   DICT_DEFAULT(std::string, protocol, "protocol", "") \
   DICT_DEFAULT(bool, negotiated, "negotiated", false) \
   DICT_OPTIONAL(uint32_t, id, "id") \
